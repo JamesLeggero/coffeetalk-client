663743d5f2e59597f7b0f5b97bc45794017dccc5
JamesLeggero <james.m.leggero@gmail.com>
2020-09-30 17:23:15 -0400

    change to server js and added proc

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3000
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+// const PORT = process.env.PORT || 3000
 
-app.use(express.static('build'))
+// app.use(express.static('build'))
+
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(`${__dirname}/build/index.html`))
+// })
+
+// //test
+
+// app.listen(PORT)
+
+require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const path = require('path')
 
 app.get('*', (req, res)=>{
     res.sendFile(path.join(`${__dirname}/build/index.html`))
 })
 
-//test
-
-app.listen(PORT)
+const port = process.env.PORT || 3001
+server.listen(port, () => console.log(`running on ${port}`))
