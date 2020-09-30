737b3401ad7e758587d6064c5a5f084a56c7b114
JamesLeggero <james.m.leggero@gmail.com>
2020-09-30 19:28:25 -0400

    added socket io client

--- server.js
+++ server.js
@@ -13,23 +13,26 @@
 
 // app.listen(PORT)
 
+import openSocket from 'socket.io-client'
+const url = 'https://jml-coffeetalk-client.herokuapp.com/'
+const socket = openSocket(url)
 require('dotenv').config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+// const socket = require("socket.io");
+// const io = socket(server);
 const path = require('path')
+
+socket.on('connect', () => {
+    console.log('conneted to', url)
+})
 
 app.use(express.static('build'))
 
 app.get('*', (req, res)=>{
     res.sendFile(path.join(`${__dirname}/build/index.html`))
-})
-
-io.on('connect', () =>{
-    console.log('connected')
 })
 
 const PORT = process.env.PORT || 3000
