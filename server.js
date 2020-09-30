f9378728a84da0a6b2d66515678836b98eacfc46
JamesLeggero <james.m.leggero@gmail.com>
2020-09-30 19:49:12 -0400

    added in old BE code

--- server.js
+++ server.js
@@ -13,21 +13,46 @@
 
 // app.listen(PORT)
 
-// import openSocket from 'socket.io-client'
-// const url = 'https://jml-coffeetalk-client.herokuapp.com/'
-// const socket = openSocket(url)
+
 require('dotenv').config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-// const socket = require("socket.io");
-// const io = socket(server);
+const socket = require("socket.io");
+const io = socket(server);
 const path = require('path')
 
-// socket.on('connect', () => {
-//     console.log('conneted to', url)
-// })
+const rooms = {};
+
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    });
+
+    socket.on("offer", payload => {
+        io.to(payload.target).emit("offer", payload);
+    });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    });
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    });
+});
+
+
 
 app.use(express.static('build'))
 
